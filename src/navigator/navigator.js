aa6f704bfad8756581522e22ffa20ad76f24667c
hiddenbrains404 <78338321+dineshchavan@users.noreply.github.com>
2021-02-28 11:37:40 +0530

    added some route

--- src/navigator/navigator.js
+++ src/navigator/navigator.js
@@ -7,13 +7,15 @@ import AboutScreen from '../screens/AboutScreen';
 import CreateTally from '../screens/CreateTally';
 import TallyPage from '../screens/TallyPage';
 import TallyStatus from '../screens/TallyStatus';
-import TabNavigation from './tabNavigator'
+import SlideView from '../screens/SlideView';
+import TabNavigation from './tabNavigator';
 const Stack = createStackNavigator();
 export default function navigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Slides">
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Slides" component={SlideView} />
         <Stack.Screen name="About" component={AboutScreen} />
         <Stack.Screen name="Tally" component={TallyPage} />
         <Stack.Screen name="CreateTally" component={CreateTally} />
--- src/screens/SlideView.js
+++ src/screens/SlideView.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Dimensions, FlatList} from 'react-native';
 import {TallyContext} from '../context/Context';
-export default function SlideView() {
+const {height, width} = Dimensions.get('window');
+export default function SlideView(props) {
   const {state, setState} = React.useContext(TallyContext);
+  React.useLayoutEffect(() => {
+    props.navigation.setOptions({
+      headerShown: false,
+    });
+  }, [props.navigation]);
+
+  const renderItem = ({item, index}) => {
+    return (
+      <View
+        style={{
+          height: height * 0.5,
+          borderWidth: 1,
+          width: width,
+          alignItems: 'center',
+        //   justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            width: width * 0.8,
+            height: height * 0.5,
+            backgroundColor: '#399',
+          }}>
+          <Text>{item}</Text>
+        </View>
+      </View>
+    );
+  };
 
   return (
-    <View>
-      <Text></Text>
+    <View
+      style={{width, height, alignItems: 'center', justifyContent: 'center'}}>
+      <View>
+        <FlatList
+          data={[1, 2, 3, 4, 5]}
+          horizontal
+          pagingEnabled
+          renderItem={renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
     </View>
   );
 }
